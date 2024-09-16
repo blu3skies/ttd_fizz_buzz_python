29b8fcbc3a0ad4fce82090d5005bbdb82ccffcfb
Joe Ellingworth <joeellingworth@Joes-MacBook-Pro-2.local>
2024-09-16 14:51:32 +0100

    altering first test to check function is is appending.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,7 @@
-
-def fizzbuzz(number):
-    return(number)
+array = []
+def fizzbuzz(x):
+    i = 1
+    while i < x+1:
+        array.append(i)
+        i += 1	
+    return array
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,4 +1,4 @@
 from fizzbuzz import fizzbuzz
 
 def test_fizzbuzz_1():
-    assert fizzbuzz("1") == "1"
+    assert fizzbuzz(2) == [1, 2]

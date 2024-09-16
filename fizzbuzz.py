f7bc09c5d8e4d4e9f56ac76b6b0df37d3271143b
Joe Ellingworth <joeellingworth@Joes-MacBook-Pro-2.local>
2024-09-16 17:27:14 +0100

    refactor to clean up

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,19 +6,12 @@ def fizzbuzz(x):
         print(i)
         if i % 15 == 0:
             array.append("FizzBuzz")
-            i += 1	
         elif i % 3 == 0:
             array.append("Fizz")
-            #print(array)
-            i += 1	
         elif i % 5 == 0:
             array.append("Buzz")
-            #print(array)
-            i += 1	
         else:
             array.append(i)
-            #print(array)
-            i += 1	
-        #print(array)
+        i += 1	
     return array
 
